2fd38afa38d251350b3a2043a6aee57598b02669
EleisonC <ckalule7@gmail.com>
2025-05-13 17:16:27 +0300

    refactor logging

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,6 @@ use p2p_node_handshake::{HandTool, utils};
 #[tokio::main]
 async fn main() -> Result<()> {
     utils::hand_logger::init_logger();
-    tracing::info!("Welcome to the Handshake");
-    tracing::info!("Pre configure with two Testnet Bitcoin public nodes:\
-        testnet-seed.bitcoin.jonasschnelli.ch:18333\
-        seed.testnet.bitcoin.sprovoost.nl:18333\
-        Default number of handshake attempts is 2
-    ");
     let mut tool = HandTool::default();
 
     tool.set_max_handshake_attempts(3);
--- src/utils/hand_logger.rs
+++ src/utils/hand_logger.rs
@@ -7,4 +7,11 @@ pub fn init_logger() {
         .with_file(false)
         .with_line_number(false)
         .init();
+
+    tracing::info!("######## Welcome to HS ########");
+    tracing::info!("Pre configure with two Testnet Bitcoin public nodes:\
+        testnet-seed.bitcoin.jonasschnelli.ch:18333\
+        seed.testnet.bitcoin.sprovoost.nl:18333\
+        Default number of handshake attempts is 2
+    ");
 }
